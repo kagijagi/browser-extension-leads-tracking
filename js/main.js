bc4199877ec3c9800ac11e3e6d29f404e799d691
KagiJagi <44635879+kagijagi@users.noreply.github.com>
2026-01-03 23:20:00 +0700

    Added functionality with localStorage.
    Added local storage set/get.
    Added delete links button.

--- js/main.js
+++ js/main.js
@@ -4,12 +4,31 @@ let myLeads = [];
 // Get references to DOM elements
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
+const deleteBtn = document.getElementById("delete-btn");
 const ulEl = document.getElementById("ul-el");
 
-// Add event listener to the button
+// Retrieve leads from local storage if available
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+
+// If leads exist in local storage, update myLeads and render them
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage;
+    renderLeads();
+}
+
+// Add event listener to the button to save input
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value);
     inputEl.value = "";
+
+    localStorage.setItem("myLeads", JSON.stringify(myLeads));
+    renderLeads();
+})
+
+// Add event listener to the button to delete all leads
+deleteBtn.addEventListener("dblclick", function() {
+    myLeads = [];
+    localStorage.clear();
 
     renderLeads();
 })
@@ -18,14 +37,16 @@ inputBtn.addEventListener("click", function() {
 function renderLeads() {
     let listItems = "";
 
-    for (let lead of myLeads) {
-        listItems += `
-            <li>
-                <a target='_blank' href='${lead}'>
-                    ${lead}
-                </a>
-            </li>
-        `;
+    if (myLeads.length !== 0) {
+        for (let lead of myLeads) {
+            listItems += `
+                <li>
+                    <a target='_blank' href='${lead}'>
+                        ${lead}
+                    </a>
+                </li>
+            `;
+        }
     }
 
     ulEl.innerHTML = listItems;
