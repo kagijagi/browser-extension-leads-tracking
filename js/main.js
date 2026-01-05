3e0052c27cafcbe7b592ac0f08c36d90c43c36dd
KagiJagi <44635879+kagijagi@users.noreply.github.com>
2026-01-05 20:41:13 +0700

    Final version changes
    Added saving current tab URL using Chrome API

--- js/main.js
+++ js/main.js
@@ -3,8 +3,11 @@ let myLeads = [];
 
 // Get references to DOM elements
 const inputEl = document.getElementById("input-el");
+
 const inputBtn = document.getElementById("input-btn");
 const deleteBtn = document.getElementById("delete-btn");
+const tabBtn = document.getElementById("tab-btn");
+
 const ulEl = document.getElementById("ul-el");
 
 // Retrieve leads from local storage if available
@@ -13,32 +16,15 @@ const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
 // If leads exist in local storage, update myLeads and render them
 if (leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage;
-    renderLeads();
+    render(myLeads);
 }
 
-// Add event listener to the button to save input
-inputBtn.addEventListener("click", function() {
-    myLeads.push(inputEl.value);
-    inputEl.value = "";
-
-    localStorage.setItem("myLeads", JSON.stringify(myLeads));
-    renderLeads();
-})
-
-// Add event listener to the button to delete all leads
-deleteBtn.addEventListener("dblclick", function() {
-    myLeads = [];
-    localStorage.clear();
-
-    renderLeads();
-})
-
 // Render the leads to the unordered list
-function renderLeads() {
+function render(leads) {
     let listItems = "";
 
-    if (myLeads.length !== 0) {
-        for (let lead of myLeads) {
+    if (leads.length !== 0) {
+        for (let lead of leads) {
             listItems += `
                 <li>
                     <a target='_blank' href='${lead}'>
@@ -51,3 +37,35 @@ function renderLeads() {
 
     ulEl.innerHTML = listItems;
 }
+
+// Add event listener to the button to save input
+inputBtn.addEventListener("click", function() {
+    myLeads.push(inputEl.value);
+    inputEl.value = "";
+
+    localStorage.setItem("myLeads", JSON.stringify(myLeads));
+    render(myLeads);
+})
+
+// Add event listener to the button to save the current tab URL
+tabBtn.addEventListener("click", function() {
+    // Use Chrome Tabs API to get the active tab
+    chrome.tabs.query(
+        {active: true, currentWindow: true}, // Query for the active tab in the current window
+        function(tabs) { // Callback function to handle the result
+            const activeTab = tabs[0];
+            myLeads.push(activeTab.url);
+
+            localStorage.setItem("myLeads", JSON.stringify(myLeads));
+            render(myLeads);
+        }
+    );
+});
+
+// Add event listener to the button to delete all leads
+deleteBtn.addEventListener("dblclick", function() {
+    myLeads = [];
+    localStorage.clear();
+
+    render(myLeads);
+})
